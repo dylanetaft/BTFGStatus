b9087d0fe0e8f785560257b67cabf268f5fadb38
Dylan Taft <dylanetaft@gmail.com>
2018-01-06 01:13:13 -0500

    more testing, switch to cJson, libcurl,testing fltk

--- main.cpp
+++ main.cpp
@@ -1,50 +1,81 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <curl/curl.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <functional>
+#include <cjson/cJSON.h>
 
-#include "Poco/Net/HTTPClientSession.h"
-#include "Poco/Net/HTTPRequest.h"
-#include "Poco/Net/HTTPResponse.h"
-#include <Poco/Net/HTTPCredentials.h>
-#include "Poco/StreamCopier.h"
-#include "Poco/NullStream.h"
-#include "Poco/Path.h"
-#include "Poco/URI.h"
-#include "Poco/Exception.h"
-#include "Poco/JSON/Object.h"
-#include "Poco/JSON/Parser.h"
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
 
-
-void doRequest(Poco::Net::HTTPClientSession& session, Poco::Net::HTTPRequest& request, Poco::Net::HTTPResponse& response) {
-	session.sendRequest(request);
-	std::istream& rs = session.receiveResponse(response);
-	if (response.getStatus() != Poco::Net::HTTPResponse::HTTP_UNAUTHORIZED) {
-		std::string json_data;
-		Poco::StreamCopier::copyToString(rs,json_data);
-		Poco::JSON::Parser parser;
-		auto res = parser.parse(json_data);
-		auto obj = res.extract<Poco::JSON::Object::Ptr>();
+std::vector<char> getRestJSONData() {
+	std::vector<char> http_data;
 	
-		auto pendingPaymentList = obj->getObject("pendingPaymentList");
-		if (pendingPaymentList.get() == nullptr) throw std::runtime_error("Unexpected HTTP Data Response"); 
-		if (pendingPaymentList->has("4058609956838583597")) {
-			auto val = pendingPaymentList->getValue<std::string>("4058609956838583597");
-			std::cout << val << "\n";
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL,"http://burst.btfg.space/pool-payments.json");
+		
+		
+		auto cb_lambda = [](void *buffer, size_t size, size_t nmemb, void *stream) {
+			auto *http_data = (std::vector<char> *) stream;
+			
+			int len = size * nmemb;
+			char *tbuf = (char *)buffer;
+			std::vector<char> t_data;
+			t_data.assign(tbuf , tbuf + len);
+			http_data->insert(http_data->end(),t_data.begin(), t_data.end());
+			return len;				
+		};
+		int (*fptr)(void *, size_t, size_t, void*);
+		fptr = cb_lambda;
+
+		
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &http_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fptr);
+		res = curl_easy_perform(curl);
+		if (res == CURLE_OK) {
 		}
 		else {
-			throw std::runtime_error("Account ID does not have any pending payment.");  
+			std::cout << res << " " << curl_easy_strerror(res) << "\n";
+			
 		}
+		curl_easy_cleanup(curl);
+	}	
+	return http_data;
+}
+
+double getPendingPayment(std::vector<char> json_data, const std::string &account_id) {
+	json_data.push_back('\0');
+	double val = 0;
+	if (json_data.size() > 0) {
+		cJSON *json = cJSON_Parse(&json_data[0]);
+		cJSON *pending_list_obj = cJSON_GetObjectItem(json, "pendingPaymentList");
+		if (cJSON_IsObject(pending_list_obj)) {
+				cJSON *item = cJSON_GetObjectItem(pending_list_obj,account_id.c_str());
+				if (item != NULL) {
+					val = item->valuedouble;
+				}
+		}
+		cJSON_Delete(json);	
 	}
-	else {
-		throw std::runtime_error("Unexpected HTTP Response");
-	}
+	return val; //actually throw an error
 }
 int main() {
-	  Poco::URI uri("http://burst.btfg.space");
-		Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
-		Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, "/pool-payments.json", Poco::Net::HTTPMessage::HTTP_1_1);
-		Poco::Net::HTTPResponse response;
-		doRequest(session, request, response);
-		std::cout << "Hello\n";
-		return 0;
+		//auto data = getRestJSONData();
+		//std::cout << getPendingPayment(data, "4058609956838583597");
+		Fl_Window window(1024,768);
+		Fl_Box box(20,40,300,200,"Hello!");
+		box.box(FL_UP_BOX);
+		box.labelfont(FL_BOLD+FL_ITALIC);
+		box.labelsize(36);
+		box.labeltype(FL_SHADOW_LABEL);
+		window.end();
+		window.show();
+		return Fl::run();
 }
